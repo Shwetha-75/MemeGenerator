969df10f4ee37745b86781898e64d8e26a86b5da
Shwetha K <105779009+Shwetha-75@users.noreply.github.com>
2024-03-19 18:27:11 +0530

    Update content.js

--- meme/src/Component/content.js
+++ meme/src/Component/content.js
@@ -2,15 +2,27 @@ import React from 'react'
 import data from './image'
 export default function Content()
 {
-    
+   
+
+    const [image,setImage]=React.useState();
+
+    const handleOnClick=()=>
+    {
+        const images=[...data]
+        const random=Math.floor(Math.random()*(images.length));
+        console.log(random);
+        setImage(images[random]);
+    }
     return(
         <div className='meme-tag'>
-            <form className='form-tag'>
+            <div className='form-tag'>
               <input type="text" placeholder='Top Content' className='input1-tag'></input>
               <input type="text" placeholder='Bottom Content' className='input2-tag'></input><br></br>
-              <button>GET ME IMAGE</button>
-            </form>
-            <img></img>
+              <button onClick={handleOnClick}>GET ME IMAGE</button>
+              <img src={image} alt=''></img>
+            </div>
+            
+            
             
         </div>
 
